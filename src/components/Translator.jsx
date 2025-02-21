bf76ad841b5a54424628863d4e86907c87dd3580
Yashwanth2424 <thalkayashwanth1234@gmail.com>
2025-02-21 15:19:59 +0530

    changes

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Loader from './Loader';
+import Loader from './Loader/Loader';
 
 function Translator() {
       const [textInput,setTextInput] = useState("")
